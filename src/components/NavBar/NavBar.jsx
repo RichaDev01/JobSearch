ecb2d64dd7e41332e24e6fd1dddd1503f29d463b
Richa Garg <138007104+RichaDev01@users.noreply.github.com>
2023-07-17 11:16:29 +0530

    Update NavBar.jsx

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -5,7 +5,7 @@ const NavBar = () => {
     <div className='navBar flex justify-between items-center p-[3rem]'>
       <div className='logoDiv'>
         <h1 className='logo text-[25px] text-blueColor'>
-        <strong>Job</strong>Search</h1>
+        <strong>FingerTip</strong>Jobs</h1>
       </div>
       
       <div className='menu flex gap-8'>
